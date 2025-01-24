68d56defc88d6a5321a3a2bc0d559eaf91af5c5e
Juan David Lasso <juandavid.lasso@dialapplet.com>
2025-01-24 14:25:06 -0500

    proyecto nuevo

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-	images: {
-		unoptimized: true
-	},
+    images: {
+        unoptimized: true
+    },
     async redirects() {
         return [{ source: '/', destination: '/user/login', permanent: true }];
     }
